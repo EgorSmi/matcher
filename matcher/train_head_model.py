9547f03bc8634a202e557c62451bd4bc315c5f7e
egor.smirnov <egor.smirnov@aliexpress.ru>
2023-05-23 20:00:12 +0300

    add label for training head model

--- matcher/train_head_model.py
+++ matcher/train_head_model.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import torch
 
-from matcher.feature_register import FeatureRegister
 from matcher.features.config import Config
+from matcher.feature_register import FeatureRegister
 from matcher.head_model.model import CatboostModel
 from matcher.head_model.catboost_experiment import CatboostConfig
 
 
 def train_head_model(
-    train_features_df: pd.DataFrame, eval_features_df: pd.DataFrame,
+    train_features_df: pd.DataFrame, eval_features_df: pd.DataFrame, all_train_pairs_df: pd.DataFrame,
     label_column: str, save_path: str):
-    config = Config()
-    feature_register = FeatureRegister(config)
+    feature_config = Config()
+    feature_register = FeatureRegister(feature_config)
+
+    train_features_df = train_features_df.merge(all_train_pairs_df, on=feature_register.pair_key_columns, how="inner")
+    eval_features_df = eval_features_df.merge(all_train_pairs_df, on=feature_register.pair_key_columns, how="inner")
 
     task_type = "GPU" if torch.cuda.is_available() else "CPU"
     catboost_config = CatboostConfig(task_type)
@@ -21,10 +24,10 @@ def train_head_model(
         train_features_df,
         eval_features_df,
         label_column=label_column,
-        feature_columns=feature_register.features,
+        feature_columns=feature_config.config_feature_names,
         output_path=save_path,
     )
 
-    train_pool = CatboostModel.get_pool(train_features_df, label_column, feature_register.features)
+    train_pool = CatboostModel.get_pool(train_features_df, label_column, feature_config.config_feature_names)
     feature_importance = catboost_model.get_feature_importance(train_pool)
     print(f"Feature Importance: ", feature_importance)
